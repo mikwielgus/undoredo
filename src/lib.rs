bd9973336560aa4a411f6f7dd4e39dcb1467865a
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2025-12-13 15:15:50 +0100

    Make some updates to Cargo.toml and src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 /*#![doc(html_root_url = "https://docs.rs/undoredo")]
 #![deny(missing_docs)]
-#![forbid(unsafe_code)]
-#![cfg_attr(not(feature = "std"), no_std)]*/
+#![forbid(unsafe_code)]*/
+#![cfg_attr(not(feature = "std"), no_std)]
 
 mod collection;
 mod edit;
@@ -9,6 +9,7 @@ mod impls;
 mod recorder;
 mod undoredo;
 
+pub use crate::collection::{Get, Insert, Push, Remove};
 pub use crate::edit::Edit;
 pub use crate::recorder::Recorder;
 pub use crate::undoredo::UndoRedo;

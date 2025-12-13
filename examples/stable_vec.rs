6a97a9aac63c9ed101eef5b2da04c051b9a66361
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2025-12-13 15:17:11 +0100

    Add StableVec example to examples directory

---
+++ examples/stable_vec.rs
@@ -0,0 +1,46 @@
+use stable_vec::StableVec;
+use undoredo::{Push, Recorder, Remove, UndoRedo};
+
+#[test]
+fn main() {
+    let mut recorder: Recorder<usize, char, StableVec<char>> = Recorder::new(StableVec::new());
+    let mut undoredo: UndoRedo<StableVec<char>> = UndoRedo::new();
+
+    recorder.push('A');
+    undoredo.commit(recorder.flush());
+
+    recorder.push('B');
+    recorder.push('B');
+    undoredo.commit(recorder.flush());
+
+    let key = recorder.push('X');
+    recorder.remove(&key);
+    recorder.push('C');
+    undoredo.commit(recorder.flush());
+
+    assert!(
+        recorder
+            .container()
+            .values()
+            .copied()
+            .eq(['A', 'B', 'B', 'C'])
+    );
+
+    undoredo.undo(&mut recorder);
+    assert!(recorder.container().values().copied().eq(['A', 'B', 'B']));
+
+    undoredo.undo(&mut recorder);
+    assert!(recorder.container().values().copied().eq(['A']));
+
+    undoredo.redo(&mut recorder);
+    assert!(recorder.container().values().copied().eq(['A', 'B', 'B']));
+
+    undoredo.redo(&mut recorder);
+    assert!(
+        recorder
+            .container()
+            .values()
+            .copied()
+            .eq(['A', 'B', 'B', 'C'])
+    );
+}

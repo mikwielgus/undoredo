2adcb71893b90e5ebb0d681d4e628c2ed9c1ee67
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2025-12-14 19:02:25 +0100

    Add missing copyright lines to become REUSE compliant

--- examples/basic_usage.rs
+++ examples/basic_usage.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 undoredo contributors
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
 use std::collections::HashMap;
 use undoredo::{MapInsert, Recorder, UndoRedo};
 
--- src/undoredo.rs
+++ src/undoredo.rs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 undoredo contributors
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
+
 use alloc::vec::Vec;
 
 use crate::{

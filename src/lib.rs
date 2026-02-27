6a39b6e4361dce878b63f9337e4ef9306be09105
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-02-27 18:11:16 +0100

    Release version 0.8.12 of `undoredo_derive` as well

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,7 @@ mod edit;
 mod recorder;
 mod undoredo;
 
-pub use crate::edit::ApplyEdit;
-pub use crate::edit::Edit;
+pub use crate::edit::{ApplyEdit, Edit};
 pub use crate::recorder::{FlushEdit, Recorder};
 pub use crate::undoredo::UndoRedo;
 pub use maplike::{Get, Insert, IntoIter, KeyedCollection, Map, Push, Remove, StableRemove};

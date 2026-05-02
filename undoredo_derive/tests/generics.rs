1e72847c26e179401aa798859d7e410ee40c2fa6
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-05-02 15:35:56 +0200

    Add test for delta derives on generic types in `undoredo_derive`

---
+++ undoredo_derive/tests/generics.rs
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: 2026 undoredo contributors
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+#![allow(dead_code)]
+
+use std::collections::BTreeMap;
+
+use undoredo::{ApplyDelta, FlushDelta, Recorder};
+use undoredo_derive::Delta;
+
+#[derive(Clone, Debug, Delta)]
+struct Generics<
+    C1 = Recorder<Vec<usize>>,
+    C2 = C1,
+    C3 = C1,
+> {
+    first: C1,
+    second: C2,
+    third: C3,
+}
+
+#[test]
+fn test_generics_delta_derive_with_three_containers() {
+    type RecordedVec = Recorder<Vec<usize>, BTreeMap<usize, usize>>;
+
+    let mut from = Generics {
+        first: RecordedVec::new(vec![0, 1]),
+        second: RecordedVec::new(vec![0, 0]),
+        third: RecordedVec::new(vec![1, 1]),
+    };
+    let mut to = Generics {
+        first: RecordedVec::new(vec![0, 1]),
+        second: RecordedVec::new(vec![0, 0]),
+        third: RecordedVec::new(vec![1, 1]),
+    };
+
+    from.first.set(1, 0);
+    from.second.set(0, 1);
+    from.third.set(1, 2);
+
+    let delta: undoredo::Delta<GenericsHalfDelta<RecordedVec, RecordedVec, RecordedVec>> =
+        from.flush_delta();
+    to.apply_delta(delta);
+
+    assert_eq!(to.first.container(), &vec![0, 0]);
+    assert_eq!(to.second.container(), &vec![1, 0]);
+    assert_eq!(to.third.container(), &vec![2, 2]);
+}

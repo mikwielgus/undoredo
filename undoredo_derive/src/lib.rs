df9529286b0355dde28e1fb21317785e9c51848e
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-05-11 12:09:19 +0200

    Add some documentation for derive macros

--- undoredo_derive/src/lib.rs
+++ undoredo_derive/src/lib.rs
@@ -11,24 +11,28 @@ mod half_delta;
 use proc_macro::TokenStream;
 use syn::{DeriveInput, parse_macro_input};
 
+/// Generates a half-delta type for the given `struct` or `enum`.
 #[proc_macro_derive(HalfDelta, attributes(undoredo))]
 pub fn derive_half_delta(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     half_delta::expand_half_delta(input).unwrap_or_else(|err| err.to_compile_error().into())
 }
 
+/// Generates an impl of the trait `ApplyDelta`.
 #[proc_macro_derive(ApplyDelta, attributes(undoredo))]
 pub fn derive_apply_delta(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     apply_delta::expand_apply_delta(input).unwrap_or_else(|err| err.to_compile_error().into())
 }
 
+/// Generates an impl of the trait `FlushDelta`.
 #[proc_macro_derive(FlushDelta, attributes(undoredo))]
 pub fn derive_flush_delta(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     flush_delta::expand_flush_delta(input).unwrap_or_else(|err| err.to_compile_error().into())
 }
 
+/// Generates an impl of the trait `ApplyDelta`.
 #[proc_macro_derive(Delta, attributes(undoredo))]
 pub fn derive_delta(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);

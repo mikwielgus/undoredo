e1759dc9bf7136eef26077072c18e7c68f389d7b
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-08-02 23:55:37 +0200

    Test for all supported types in `undoredo_derive`'s delta and half-delta tests

--- undoredo_derive/tests/delta.rs
+++ undoredo_derive/tests/delta.rs
@@ -2,13 +2,19 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-//! Tests for `derive(Delta)` (doc comments here are to pass deny(missing_docs)).
+//! Tests for `derive(Delta)` (doc comments here are to pass deny(missing_docs),
+//! compliance with which we also test here).
 
 #![deny(missing_docs)]
 #![allow(dead_code)]
 
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
+
 use maplike::ops::Assign;
-use undoredo::aliases::VecHalfDelta;
+use undoredo::aliases::{
+    BTreeMapHalfDelta, BTreeSetHalfDelta, HashMapHalfDelta, HashSetHalfDelta, OptionHalfDelta,
+    VecHalfDelta,
+};
 use undoredo::{ApplyDelta, Delta, Recorder, ResetDelta};
 
 /// Struct under test for `derive(Delta)`.
@@ -16,38 +22,85 @@ use undoredo::{ApplyDelta, Delta, Recorder, ResetDelta};
 #[undoredo(half_delta = TestStructHalfDelta)]
 #[undoredo(delta = TestStructDelta)]
 pub struct TestStruct {
-    v: Recorder<Vec<i32>>,
+    hashmap: Recorder<HashMap<i32, i32>, HashMapHalfDelta<i32, i32>>,
+    hashset: Recorder<HashSet<i32>, HashSetHalfDelta<i32>>,
+    btreemap: Recorder<BTreeMap<i32, i32>>,
+    btreeset: Recorder<BTreeSet<i32>>,
+    option: Recorder<Option<i32>>,
+    vec: Recorder<Vec<i32>>,
+}
+
+#[test]
+fn test_struct_delta() {
+    let _: TestStructDelta = TestStructDelta::with_removed_inserted(
+        TestStructHalfDelta {
+            hashmap: HashMapHalfDelta::new(),
+            hashset: HashSetHalfDelta::new(),
+            btreemap: BTreeMapHalfDelta::new(),
+            btreeset: BTreeSetHalfDelta::new(),
+            option: OptionHalfDelta::new(),
+            vec: VecHalfDelta::new(),
+        },
+        TestStructHalfDelta {
+            hashmap: HashMapHalfDelta::new(),
+            hashset: HashSetHalfDelta::new(),
+            btreemap: BTreeMapHalfDelta::new(),
+            btreeset: BTreeSetHalfDelta::new(),
+            option: OptionHalfDelta::new(),
+            vec: VecHalfDelta::new(),
+        },
+    );
 }
 
 #[test]
 fn test_delta_derive_struct() {
     let mut s = TestStruct {
-        v: Recorder::new(vec![1, 2, 3]),
+        hashmap: Recorder::new(HashMap::from([(1, 2)])),
+        hashset: Recorder::new(HashSet::from([1])),
+        btreemap: Recorder::new(BTreeMap::from([(1, 2)])),
+        btreeset: Recorder::new(BTreeSet::from([1])),
+        option: Recorder::new(Some(1)),
+        vec: Recorder::new(vec![1, 2, 3]),
     };
 
     let d = TestStructDelta::with_removed_inserted(
         TestStructHalfDelta {
-            v: VecHalfDelta::from([(2, 3)]),
+            hashmap: HashMapHalfDelta::new(),
+            hashset: HashSetHalfDelta::new(),
+            btreemap: BTreeMapHalfDelta::new(),
+            btreeset: BTreeSetHalfDelta::new(),
+            option: OptionHalfDelta::new(),
+            vec: VecHalfDelta::from([(2, 3)]),
         },
         TestStructHalfDelta {
-            v: VecHalfDelta::from([(2, 7)]),
+            hashmap: HashMapHalfDelta::new(),
+            hashset: HashSetHalfDelta::new(),
+            btreemap: BTreeMapHalfDelta::new(),
+            btreeset: BTreeSetHalfDelta::new(),
+            option: OptionHalfDelta::new(),
+            vec: VecHalfDelta::from([(2, 7)]),
         },
     );
     s.apply_delta(d);
 
-    assert_eq!(s.v.as_ref(), &vec![1, 2, 7]);
+    assert_eq!(s.vec.as_ref(), &vec![1, 2, 7]);
 }
 
 #[test]
 fn test_delta_derive_struct_provides_reset_delta() {
     let mut s = TestStruct {
-        v: Recorder::new(vec![1, 2, 3]),
+        hashmap: Recorder::new(HashMap::from([(1, 2)])),
+        hashset: Recorder::new(HashSet::from([1])),
+        btreemap: Recorder::new(BTreeMap::from([(1, 2)])),
+        btreeset: Recorder::new(BTreeSet::from([1])),
+        option: Recorder::new(Some(1)),
+        vec: Recorder::new(vec![1, 2, 3]),
     };
 
-    s.v.set(2, 7);
+    s.vec.set(2, 7);
     s.reset_delta();
 
-    assert_eq!(*s.v.container(), vec![1, 2, 3]);
+    assert_eq!(*s.vec.container(), vec![1, 2, 3]);
 }
 
 /// Enum under test for `derive(Delta)`.
--- undoredo_derive/tests/half_delta.rs
+++ undoredo_derive/tests/half_delta.rs
@@ -2,23 +2,40 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-//! Tests for `derive(HalfDelta)` (doc comments here are to pass deny(missing_docs)).
+//! Tests for `derive(Delta)` (doc comments here are to pass deny(missing_docs),
+//! compliance with which we also test here).
 
 #![deny(missing_docs)]
 #![allow(dead_code)]
 
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
 
+use undoredo::aliases::{
+    BTreeMapHalfDelta, BTreeSetHalfDelta, HashMapHalfDelta, HashSetHalfDelta, OptionHalfDelta,
+    VecHalfDelta,
+};
 use undoredo::{HalfDelta, Recorder};
 
 /// Struct under test.
 #[derive(HalfDelta)]
 #[undoredo(half_delta = TestStructHalfDelta)]
 pub struct TestStruct {
-    v: Recorder<Vec<i32>>,
+    hashmap: Recorder<HashMap<i32, i32>>,
+    hashset: Recorder<HashSet<i32>>,
+    btreemap: Recorder<BTreeMap<i32, i32>>,
+    btreeset: Recorder<BTreeSet<i32>>,
+    option: Recorder<Option<i32>>,
+    vec: Recorder<Vec<i32>>,
 }
 
 #[test]
 fn test_struct_half_delta() {
-    let _: TestStructHalfDelta = TestStructHalfDelta { v: BTreeMap::new() };
+    let _: TestStructHalfDelta = TestStructHalfDelta {
+        hashmap: HashMapHalfDelta::new(),
+        hashset: HashSetHalfDelta::new(),
+        btreemap: BTreeMapHalfDelta::new(),
+        btreeset: BTreeSetHalfDelta::new(),
+        option: OptionHalfDelta::new(),
+        vec: VecHalfDelta::new(),
+    };
 }
